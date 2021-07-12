f301e91c0b0b887427bc687b9ebfdaddcf851e2c
Benjamin Izrailov <benizra@benjamins-mbp.myfiosgateway.com>
2021-07-12 09:26:50 -0400

    Configure sql

--- starter-client-main/src/components/views/CampusView.js
+++ starter-client-main/src/components/views/CampusView.js
@@ -2,8 +2,10 @@ const CampusView = (props) => {
   const {campus} = props;
   return (
     <div>      
-      <h1>{campus.name}</h1>
-      <p>{campus.description}</p>
+      <h1>Name: {campus.name}</h1>
+      <h2>Description: {campus.description}</h2>
+      <img src={campus.imgurl} height = '150' width = '250'/>
+      <h3>Address: {campus.address}</h3>
       <ul>
       {campus.students.map( student => {
         let name = student.firstname + " " + student.lastname;
--- starter-client-main/src/components/views/StudentView.js
+++ starter-client-main/src/components/views/StudentView.js
@@ -3,7 +3,10 @@ const StudentView = (props) => {
   return (
     <div>
       <h1>{student.firstname + " " + student.lastname}</h1>
+      <img src={student.imgurl} height = '150' width = '250'/>
       <h3>{student.campus.name}</h3>
+      <h2>Email:{student.firstname + "" + student.lastname}@{student.campus.name}.com</h2>
+      <h3>GPA:{student.gpa}</h3>
     </div>
   );
 
